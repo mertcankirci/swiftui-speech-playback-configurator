765a23ef6c5f320c501ae42d35079422187241ff
mertcankirci <mertkirci02@gmail.com>
2025-05-02 22:07:13 +0300

    Initial.

---
+++ speech-playback-configurator/speech-playback-configurator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  speech-playback-configurator
+//
+//  Created by Mertcan Kırcı on 2.05.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ speech-playback-configurator/speech-playback-configurator/speech_playback_configuratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  speech_playback_configuratorApp.swift
+//  speech-playback-configurator
+//
+//  Created by Mertcan Kırcı on 2.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct speech_playback_configuratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
